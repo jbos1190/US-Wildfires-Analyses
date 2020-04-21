8d53c553ca6a79af5d1d93ff6e25810cf9700547
jbos1190 <3248566+jbos1190@users.noreply.github.com>
2020-04-21 01:41:19 -0400

    Modified fire dataframe to have separate date and time fields; added code to write AppEEARS request files.

--- wildfiresAnalyses.R
+++ wildfiresAnalyses.R
@@ -2,40 +2,98 @@
 library("RSQLite")
 
 con <- dbConnect(SQLite(), "FPA_FOD_20170508.sqlite")
-fireData <- dbGetQuery(con, paste("SELECT OBJECTID id, ",
-				  "DATETIME(DISCOVERY_DATE) discoveryDate, ",
-				  "DISCOVERY_TIME discoveryTime, ",
-				  "CONT_DATE containmentDate, ",
-				  "CONT_TIME containmentTime, ",
-				  "FIRE_SIZE fireSize, ",
-				  "LATITUDE latitude, ",
-				  "LONGITUDE longitude, ",
-				  "STATE state, ",
+fireData <- dbGetQuery(con, paste("SELECT OBJECTID id,",
+				  "STRFTIME('%Y-%m-%d', DISCOVERY_DATE)",
+				     "discoveryDate,",
+				  "SUBSTR(DISCOVERY_TIME, 1, 2) || ':'",
+				     "|| SUBSTR(DISCOVERY_TIME, 3, 2) || ':00'",
+				     "discoveryTime,",
+				  "STRFTIME('%Y-%m-%d', DISCOVERY_DATE) || ' '",
+				     "|| SUBSTR(DISCOVERY_TIME, 1, 2) || ':'",
+				     "|| SUBSTR(DISCOVERY_TIME, 3, 2) || ':00'",
+				     "discoveryDatetime,",
+				  "STRFTIME('%Y-%m-%d', CONT_DATE)",
+				     "containmentDate,",
+				  "SUBSTR(CONT_TIME, 1, 2) || ':'",
+				     "|| SUBSTR(CONT_TIME, 3, 2) || ':00'",
+				     "containmentTime,",
+				  "STRFTIME('%Y-%m-%d', CONT_DATE) || ' '",
+				     "|| SUBSTR(CONT_TIME, 1, 2) || ':'",
+				     "|| SUBSTR(CONT_TIME, 3, 2) || ':00'",
+				     "containmentDatetime,",
+				  "FIRE_SIZE fireSize,",
+				  "LATITUDE latitude,",
+				  "LONGITUDE longitude,",
+				  "STATE state,",
 				  "STAT_CAUSE_DESCR cause",
 				  "FROM Fires"))
-fireData$discoveryDate[1:3]
-fireData$discoveryTime[1:3]
-dateTimes <- dbGetQuery(con, "SELECT OBJECTID id, STRFTIME('%Y-%m-%d', DISCOVERY_DATE) || ' ' || SUBSTR(DISCOVERY_TIME, 1, 2) || ':' || SUBSTR(DISCOVERY_TIME, 3, 2) || ':00' AS discoveryTime FROM Fires")
-dateTimes$discoveryTime[1:3]
-ids <- fireData$id
-ids <- sort.list(ids)
-for (i in 1:(length(ids)-1))
-	if (ids[i] >= ids[i+1])
-		print("oops")
-nullDiscoveryDate <- dbGetQuery(con, paste("SELECT OBJECTID",
-					   "FROM Fires",
-					   "WHERE DISCOVERY_DATE IS NULL"))
-nrow(nullDiscoveryDate)
-nullDiscoveryTime <- dbGetQuery(con, paste("SELECT OBJECTID id",
-					   "FROM Fires",
-					   "WHERE DISCOVERY_TIME IS NULL"))
-nrow(nullDiscoveryTime)
-nullContainmentDate <- dbGetQuery(con, paste("SELECT OBJECTID id",
-					     "FROM Fires",
-					     "WHERE CONT_DATE IS NULL"));
-nrow(nullContainmentDate)
-nullContainmentTime <- dbGetQuery(con, paste("SELECT OBJECTID id",
-					 "FROM Fires",
-					 "WHERE CONT_TIME IS NULL"))
-nrow(nullContainmentTime)
+fireData$discoveryDate <- as.POSIXct(fireData$discoveryDate)
+fireData$discoveryDatetime <- as.POSIXct(fireData$discoveryDatetime)
+fireData$containmentDate <- as.POSIXct(fireData$containmentDate)
+fireData$containmentDatetime <- as.POSIXct(fireData$containmentDatetime)
+
+## confirm that id is a suitable primary key
+#nullId <- dbGetQuery(con, "SELECT OBJECTID FROM Fires WHERE OBJECTID IS NULL")
+#nrow(nullId)
+#ids <- fireData$id
+#ids <- sort.list(ids)
+#for (i in 1:(length(ids)-1))
+#	if (ids[i] >= ids[i+1])
+#		print("oops")
+#
+#naDiscoveryDatetime <- which(is.na(fireData$discoveryDatetime))
+#length(naDiscoveryDatetime)
+#
+## Find the ids that correspond to null values
+#nullDiscoveryDate <- dbGetQuery(con, paste("SELECT OBJECTID",
+#					   "FROM Fires",
+#					   "WHERE DISCOVERY_DATE IS NULL"))
+#nrow(nullDiscoveryDate)
+#nullDiscoveryTime <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#					   "FROM Fires",
+#					   "WHERE DISCOVERY_TIME IS NULL"))
+#nrow(nullDiscoveryTime)
+#nullContainmentDate <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#					     "FROM Fires",
+#					     "WHERE CONT_DATE IS NULL"));
+#nrow(nullContainmentDate)
+#nullContainmentTime <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#					     "FROM Fires",
+#					     "WHERE CONT_TIME IS NULL"))
+#nrow(nullContainmentTime)
+#nullContainmentDateOnly <- dbGetQuery(con,paste("SELECT OBJECTID id",
+#						"FROM Fires",
+#						"WHERE CONT_TIME IS NOT NULL",
+#						"AND CONT_DATE IS NULL"))
+#nrow(nullContainmentDateOnly)
+#nullFireSize <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#				      "FROM Fires",
+#				      "WHERE FIRE_SIZE IS NULL"))
+#nrow(nullFireSize)
+#nullLatitude <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#				      "FROM Fires",
+#				      "WHERE LATITUDE IS NULL"))
+#nrow(nullLatitude)
+#nullLongitude <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#				       "FROM Fires",
+#				       "WHERE LONGITUDE IS NULL"))
+#nrow(nullLongitude)
+#nullState <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#				   "FROM Fires",
+#				   "WHERE STATE IS NULL"))
+#nrow(nullState)
+#nullCause <- dbGetQuery(con, paste("SELECT OBJECTID id",
+#				   "FROM Fires",
+#				   "WHERE STAT_CAUSE_DESCR IS NULL")) 
+#nrow(nullCause)
+
+# Create the AppEEARS data-request files
+for (i in c(2011:2015)){
+	as.character(i)
+	write.table(fireData[format(fireData$discoveryDate, "%Y") == as.character(i),
+		  c("latitude", "longitude")],
+		  file = paste("AppEEARS_request_", i-1, ".csv", sep=""),
+		  sep = ",")
+}
+
 dbDisconnect(con)
